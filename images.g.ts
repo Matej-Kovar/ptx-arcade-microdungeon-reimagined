96abf1ff94b6a2463783bb6471bca6efb56b9817
Matěj Kovář <146720349+Matej-Kovar@users.noreply.github.com>
2025-04-23 14:26:08 +0200

    added basic assets

---
+++ images.g.ts
@@ -0,0 +1,91 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "tile_empty":return img`
+f f f f f f f f 
+f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 
+f f f f f f f f 
+`;
+            case "image2":
+            case "tile_entrypoint":return img`
+f f f f f f f f 
+f c c c c c c f 
+f c b c c c c f 
+f c b b c c c f 
+f c b b b c c f 
+f c b b b b c f 
+f c c c c c c f 
+f f f f f f f f 
+`;
+            case "image3":
+            case "tile_exitpoint":return img`
+f f f f f f f f 
+f c c c c c c f 
+f c c c c b c f 
+f c c c b b c f 
+f c c b b b c f 
+f c b b b b c f 
+f c c c c c c f 
+f f f f f f f f 
+`;
+            case "image5":
+            case "tile_wall":return img`
+f f f f f f f f 
+f c c c c c c f 
+f c c c c c c f 
+f c c c c c c f 
+f c c c c c c f 
+f c c c c c c f 
+f c c c c c c f 
+f f f f f f f f 
+`;
+            case "image6":
+            case "tile__floor":return img`
+f f f f f f f f 
+f b b b b b b f 
+f b b b b b b f 
+f b b b b b b f 
+f b b b b b b f 
+f b b b b b b f 
+f b b b b b b f 
+f f f f f f f f 
+`;
+            case "image4":
+            case "tile_path":return img`
+f f f f f f f f 
+f 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 f 
+f f f f f f f f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
